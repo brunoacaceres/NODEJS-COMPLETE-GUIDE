1cb2e9ffb1a87f93f058a682a76dfbbe033e3873
Bruno Caceres <brunocaceres@MacBook-Pro-de-Bruno.local>
2019-01-27 11:46:59 -0300

    Extracting User Data From the Auth Token.

--- Session28/backend/graphql/resolvers.js
+++ Session28/backend/graphql/resolvers.js
@@ -61,6 +61,11 @@ module.exports = {
     },
 
     createPost: async function({ postInput }, req) {
+        if(!req.isAuth){
+            const error = new Error('Not authenticated!');
+            error.code = 401;
+            throw error;
+        }
         const errors = [];
         if(validator.isEmpty(postInput.title) || !validator.isLength(postInput.title, {min: 5})) {
             errors.push({ message: 'Title is invalid.'});
@@ -74,14 +79,22 @@ module.exports = {
             error.code = 422;
             throw error;
         }
+        const user = await User.findById(req.userId);
+        if(!user){
+            const error = new Error('Invalid user');
+            error.data = errors;
+            error.code = 401;
+            throw error;
+        }
         const post = new Post({
             title: postInput.title,
             content: postInput.content,
-            imageUrl: postInput.imageUrl
+            imageUrl: postInput.imageUrl,
+            creator: user
         });
 
         const createdPost = await post.save();
-        // Add post to users post
+        user.posts.push(createdPost);
         return {...createdPost._doc, _id: createdPost._id.toString(), createdAt: createdPost.createdAt.toISOString(), updatedAt: createdPost.updatedAt.toISOString() }
     }
 };
